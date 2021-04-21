bd80dc311ab77df1aee4b522006c5114595fdd00
CptSchnitz <12687466+CptSchnitz@users.noreply.github.com>
2021-04-21 08:41:39 +0300

    refactor(tracing): simplified building the args for the logfn

--- src/tracing/loggerHook.ts
+++ src/tracing/loggerHook.ts
@@ -3,22 +3,26 @@ import { LogFn, Logger } from 'pino';
 
 // ignored because its the same type needed for LogFn
 // eslint-disable-next-line @typescript-eslint/ban-types, @typescript-eslint/no-explicit-any
-export type Args = [obj: object, msg?: string, ...args: any[]] | [msg: string, ...args: any[]];
+export type LogFnArgs = [obj: object, msg?: string, ...args: any[]] | [msg: string, ...args: any[]];
 
-export function logMethod(this: Logger, args: Args, method: LogFn, level: number): void {
+// disabled because this function signature is required by pino
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function logMethod(this: Logger, args: LogFnArgs, method: LogFn, level: number): void {
   const span = getSpan(context.active());
 
   if (!span) {
-    method.apply<Logger, Args, void>(this, args);
+    method.apply<Logger, LogFnArgs, void>(this, args);
     return;
   }
   const traceObj = { spanId: span.context().spanId, traceId: span.context().traceId };
 
-  if (typeof args[0] === 'object') {
-    const [obj, ...rest] = args;
-    method.apply<Logger, Args, void>(this, [{ ...obj, ...traceObj }, ...rest]);
+  let logFnArgs: LogFnArgs;
+  const [firstArg, ...rest] = args;
+  if (typeof firstArg === 'object') {
+    logFnArgs = [{ ...firstArg, ...traceObj }, ...rest];
   } else {
-    const [msg, ...rest] = args;
-    method.apply<Logger, Args, void>(this, [traceObj, msg, ...rest]);
+    logFnArgs = [traceObj, firstArg, ...rest];
   }
+
+  method.apply<Logger, LogFnArgs, void>(this, logFnArgs);
 }
