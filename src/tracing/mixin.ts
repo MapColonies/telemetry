85eac209c768bf14771c1c2540f90c4be9603683
CptSchnitz <12687466+CptSchnitz@users.noreply.github.com>
2022-05-30 10:51:32 +0300

    fix: pr fixes

--- src/tracing/mixin.ts
+++ src/tracing/mixin.ts
@@ -1,8 +1,12 @@
-/* eslint-disable */
 import { trace, context, isSpanContextValid } from '@opentelemetry/api';
 
-export function getOtelMixin() {
-  return function otelMixin(_context: object, level: number) {
+const HEXADECIMAL_BASE = 16;
+
+/* eslint-disable @typescript-eslint/ban-types */
+export function getOtelMixin(): (mergeObject: object, level: number) => object {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  return function otelMixin(_context: object, level: number): object {
+    /* eslint-enable  @typescript-eslint/ban-types */
     const span = trace.getSpan(context.active());
 
     if (!span) {
@@ -16,9 +20,11 @@ export function getOtelMixin() {
     }
 
     const record = {
+      /* eslint-disable @typescript-eslint/naming-convention */
       trace_id: spanContext.traceId,
       span_id: spanContext.spanId,
-      trace_flags: `0${spanContext.traceFlags.toString(16)}`,
+      trace_flags: `0${spanContext.traceFlags.toString(HEXADECIMAL_BASE)}`,
+      /* eslint-enable @typescript-eslint/naming-convention */
     };
 
     return record;
